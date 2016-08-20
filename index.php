75c8a1b36c27888f9ff3084bc253363019e08fea
Mahdi <mahdib9@users.noreply.github.com>
2016-08-20 11:28:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'libs/Youtube.php';
 use Smoqadam\Telegram;
 use Smoqadam\Youtube;
 
-$api_token = 'API_TOKEN';
+$api_token = '230045995:AAGcazTLWdKcBKDczV2gMvfPsS4Ew67P10Q';
 
 $tg = new Telegram($api_token);
 $y = new Youtube();
